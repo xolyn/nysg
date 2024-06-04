71f1c8d9d6d44620d58fed4c8bbabd4cc9a3675c
Lingyu Zhou <zly@dr.com>
2024-06-03 23:17:17 -0400

    new

--- w4/readme.md
+++ w4/readme.md
@@ -2,8 +2,8 @@
 ## `.csv`:
 - `arcgis.csv`, `geodb.csv` are original dataset and has never been modified.
 - others are dataframe produced during data processing and analysis.
-## `upload.cs`:
-This csharp file is written by Chris, minor change has been made for readability. It's is an example only, showing how should the uploading fields look like. The Id and Hash is not usable anyway.
+## `upload.cs`, `upload.old.cs`,`upload.orig.cs`:
+Csharp files originally written by [Chris](https://github.com/pugwonk), minor change has been made for readability for `upload.cs`. It's is an example only, showing how should the uploading fields look like. The Id and Hash is not usable anyway.
 ## `.ipynb`:
 - `w3.ipynb`: code from previous week for convenience, minor or no changes compared with [../w3/w3.ipynb](../w3/w3.ipynb)
 - `is.ipynb`: code from previous week for convenience, minor or no changes compared with [../w3/image_scraping.ipynb](../w3/image_scraping.ipynb)
--- w4/upload.orig.cs
+++ w4/upload.orig.cs
@@ -1,6 +1,3 @@
-// This file is an example only, showing how should the uploading fields look like. 
-// The Id and Hash is not usable 
-
 using Newtonsoft.Json;
 using System.Drawing;
 using System.Net.Http;
@@ -18,8 +15,8 @@ internal class Program
     // This is not really intended to be a public API and so there isn't any type checking - please
     // make sure dates, times etc are valid in the formats below.
 
-    private static readonly string myCoastUserId = 9860; // MyCoast userId
-    private static readonly string myCoastuserHash = "$P$BQQLTdlS2/AO.60WWx4A9hfVAlR06o1"; // MyCoast userId password hash
+    private static readonly string myCoastUserId = "123"; // MyCoast userId
+    private static readonly string myCoastuserHash = "123456789abcdef"; // MyCoast userId password hash
 
     private static async Task Main(string[] args)
     {
@@ -29,7 +26,7 @@ internal class Program
             using (MultipartFormDataContent formData = new MultipartFormDataContent())
             {
                 // All images must be JPEGs with a .jpg extension
-                using (FileStream stream = new FileStream(@"D:\NSYGimg", FileMode.Open, FileAccess.Read))
+                using (FileStream stream = new FileStream(@"C:\Photo_Hudson.jpg", FileMode.Open, FileAccess.Read))
                 {
                     formData.Add(new StreamContent(stream), "upfile", "upfile");
                     HttpResponseMessage fResponse = await client.PostAsync($"https://mycoast.org/blueurchin-js/appthings.php?apiv=5&user={myCoastUserId}&hash={myCoastuserHash}/&upload=image&id={imageId}&purpose=0", formData);
@@ -39,13 +36,13 @@ internal class Program
             }
 
             kvPair[] myReport = new kvPair[] {
-                new kvPair { name = "_gform-form-id", value = 72 }, 
-                new kvPair { name = "from_device", value = "Lingyu uploading 1" }, 
-                new kvPair { name = "photo_date", value = "2023-10-03" }, // yyyy-mm-dd
-                new kvPair { name = "photo_time", value = "10:26 am" }, // hh:mm a/pm
-                new kvPair { name = "image1", value = new string[] { imageId } }, // note: must be in list, otherwise server display as NaN
-                new kvPair { name = "guessSource", value = new string[] { "Ocean", "Bay" } }, 
-                new kvPair { name = "guessCause", value = new string[] { "High Tide", "Storm Surge" } }, 
+                new kvPair { name = "_gform-form-id", value = 72 }, // the internal form ID for NJ High Water
+                new kvPair { name = "from_device", value = "Chris' bulk uploader" }, // please set this to be whatever you're calling your uploader (it's just for logging)
+                new kvPair { name = "photo_date", value = "2023-10-03" }, // date format is important. All dates and times are local for the report
+                new kvPair { name = "photo_time", value = "10:26 am" }, // time format is important (and yes, I know it's a terrible one)
+                new kvPair { name = "image1", value = new string[] { imageId } }, // array of IDs of uploaded images above
+                new kvPair { name = "guessSource", value = new string[] { "Ocean", "Bay" } }, // These can be: "Ocean", "Bay", "River/Stream"
+                new kvPair { name = "guessCause", value = new string[] { "High Tide", "Storm Surge" } }, // These can be: "High Tide", "Storm Surge", "Rain"
                 new kvPair { name = "post_comment", value = "My post comment" },
                 new kvPair { name = "location_longitude", value = -122.3181386 },
                 new kvPair { name = "location_latitude", value = 47.6226932 },
